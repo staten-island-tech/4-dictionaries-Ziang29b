85068faef291b2b18f583ce6c708e80f14dba072
Ziang29b <Ziangc2@nycstudents.net>
2025-11-03 06:26:34 -0800

    fa

--- test.py
+++ test.py
@@ -1,9 +1,32 @@
-apple = {"price":"2 dollar","size" : "about 3 inches", "color": "green, red or yellow"}
-banana = {"price": "2 dollar", "size": "6 or 7 inches", "color" : "yellow or green"}
-giant_baguette_pillow = {"rice" : "222 dollars", "size": "10 feet long 1 feet 6 inches wide", "color": "baguette colored"}
-stuff = {"apple":apple,"banana":banana, "giant_baguette_pillow":giant_baguette_pillow }
-user_input = input("what do you want: ")
-if user_input in stuff:      
-    print (stuff[user_input])
-else:
-    print ("we no have")
+stuff=[ 
+                {
+                "price":"2 dollar",
+               "size" : "about 3 inches", 
+               "name": "apple"
+               },
+               {
+                "price": "2 dollar",
+                "size": "6 or 7 inches",
+                "name" : "banana"
+                },
+                {
+                "price" : "222 dollars", 
+                 "size": "10 feet long 1 feet 6 inches wide", 
+                 "name": "giant baguette pillow"
+                }
+                ]
+
+shopping = True 
+while shopping:
+        user_input = input("what do you want, banana apple or giant baguette pillow: ").lower()
+        if user_input in stuff:      
+            print (stuff[user_input])
+        else:
+            print ("we no have")
+
+        user_input = input ("are you still shopping?").lower()
+        if user_input in ["yea" , "yes", "yeah"]:
+            print ("okay enjoy your shopping")
+        else: 
+            print ("cashier is up front")
+        break 
