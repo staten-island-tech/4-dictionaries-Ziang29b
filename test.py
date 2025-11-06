29ec31e0efe656b3380b8a9d6c9a7d3d5f2d8826
Ziang29b <Ziangc2@nycstudents.net>
2025-11-06 06:14:20 -0800

    ghjk

--- test.py
+++ test.py
@@ -19,15 +19,17 @@ stuff=[
 shopping = True 
 while shopping:
         user_input = input("what do you want, banana apple or giant baguette pillow: ").lower()
-        if user_input in ["name"]:
-            print (stuff)
+        if user_input in [item["name"] for item in stuff]:
+             for item in stuff:
+                if item["name"] == user_input:
+                    print(item)
 
 
         else: 
             print("we no have")
         user_input = input ("are you still shopping?").lower()
-        if user_input in ["yea" , "yes", "yeah"].lower():
+        if user_input in ["yea" , "yes", "yeah", "yep" , "yuh"]:
             print ("okay enjoy your shopping")
         else: 
             print ("cashier is up front")
-        break 
+            break 
